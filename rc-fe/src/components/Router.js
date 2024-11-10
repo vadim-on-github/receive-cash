8a88d8da9c0332889b5f07654afe322f5e3d0d76
Vadim H <vadim@haiduk.org>
2024-11-10 18:10:46 -0500

    Refactor component, adjust wording
    Refactored the `Credits` component to `About`, cleaned up code structure, and adjusted the router to reflect these changes.
    Rearranged footer menu.

--- rc-fe/src/components/Router.js
+++ rc-fe/src/components/Router.js
@@ -3,32 +3,32 @@ import React from "react";
 import App from "./App";
 
 function AppWrapper(props) {
-    const params = useParams();
-    const navigate = useNavigate();
-    return (
-        <App params={params} navigate={navigate} body={props.body}/>
-    );
+  const params = useParams();
+  const navigate = useNavigate();
+  return (
+    <App params={params} navigate={navigate} body={props.body}/>
+  );
 }
 
 const Router = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route index path={"/"}
-                       element={<AppWrapper body='home'/>}/>
-                <Route path={"/:pageSlug"}
-                       element={<AppWrapper body='cryptos'/>}/>
-                <Route path={"/:pageSlug/:cryptoSymbol"}
-                       element={<AppWrapper body='cryptos'/>}/>
-                <Route path={"/user/pages"}
-                       element={<AppWrapper body='userPages'/>}/>
-                <Route path={"/privacy"}
-                       element={<AppWrapper body='privacy'/>}/>
-                <Route path={"/credits"}
-                       element={<AppWrapper body='credits'/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path={"/"}
+               element={<AppWrapper body='home'/>}/>
+        <Route path={"/:pageSlug"}
+               element={<AppWrapper body='cryptos'/>}/>
+        <Route path={"/:pageSlug/:cryptoSymbol"}
+               element={<AppWrapper body='cryptos'/>}/>
+        <Route path={"/user/pages"}
+               element={<AppWrapper body='userPages'/>}/>
+        <Route path={"/privacy"}
+               element={<AppWrapper body='privacy'/>}/>
+        <Route path={"/about"}
+               element={<AppWrapper body='about'/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default Router
